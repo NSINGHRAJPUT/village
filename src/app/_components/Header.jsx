a73abc0b951072878c0a48458fce69d6560cce0c
virugamacoder <virugama0@gmail.com>
2024-12-10 17:15:06 +0530

    Header Transition

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 import logo from "./../../assets/logo.png";
 export default function Header() {
-  const navbarRef = useRef(null);  // For the mobile menu container
-  const mobileMenuButtonRef = useRef(null);  // For the mobile menu toggle button
-  
+  const navbarRef = useRef(null); // For the mobile menu container
+  const mobileMenuButtonRef = useRef(null); // For the mobile menu toggle button
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const pathname = usePathname();
 
@@ -20,8 +20,10 @@ export default function Header() {
     const handleClickOutside = (event) => {
       // Check if click is outside the menu or the toggle button
       if (
-        navbarRef.current && !navbarRef.current.contains(event.target) && 
-        mobileMenuButtonRef.current && !mobileMenuButtonRef.current.contains(event.target)
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target) &&
+        mobileMenuButtonRef.current &&
+        !mobileMenuButtonRef.current.contains(event.target)
       ) {
         setMobileMenuOpen(false);
       }
@@ -43,48 +45,64 @@ export default function Header() {
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5">
             <Image
-            src={logo} // Path to the image
-            alt="logo"
-            className="h-10 w-56"
-          />
+              src={logo} // Path to the image
+              alt="logo"
+              className="h-10 w-56"
+            />
           </Link>
         </div>
         <div className="flex lg:hidden">
           <button
-            ref={mobileMenuButtonRef}  // Assign ref to the button
+            ref={mobileMenuButtonRef} // Assign ref to the button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={toggleDropdown}
           >
             <span className="sr-only">Open main menu</span>
-            <Icon icon="ic:round-menu" className="text-4xl text-[#ffffff]" />
+            <Icon
+              icon={`${mobileMenuOpen ? "line-md:close" : "ic:round-menu"}`}
+              className={`text-4xl text-[#ffffff] transition-transform duration-300 ${
+                mobileMenuOpen ? "rotate-180" : "rotate-0"
+              }`}
+            />
           </button>
         </div>
         <div className="hidden lg:flex text-[#ffffff] lg:gap-x-8 lg:justify-end font-karma">
-          <Link href="/" className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]">
-            હોમ 
+          <Link
+            href="/"
+            className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]"
+          >
+            હોમ
           </Link>
-          <Link href="/family" className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]">
-            પરિવાર 
+          <Link
+            href="/family"
+            className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]"
+          >
+            પરિવાર
           </Link>
-      
-          <Link href="/gallery" className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]">
-            ગેલેરી 
+
+          <Link
+            href="/gallery"
+            className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]"
+          >
+            ગેલેરી
           </Link>
-         
         </div>
       </nav>
 
       {/* Mobile Menu for Small Screens */}
       {mobileMenuOpen && (
-        <div ref={navbarRef} className="lg:hidden bg-white/95 text-primary p-6 flex flex-col items-center">
-        <style>{`body { overflow: hidden; }`}</style>
+        <div
+          ref={navbarRef}
+          className="lg:hidden bg-white/95 text-primary p-6 flex flex-col items-center"
+        >
+          <style>{`body { overflow: hidden; }`}</style>
           <Link
             href="/"
             className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]"
             onClick={() => setMobileMenuOpen(false)}
           >
-           હોમ 
+            હોમ
           </Link>
 
           <Link
@@ -92,20 +110,16 @@ export default function Header() {
             className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]"
             onClick={() => setMobileMenuOpen(false)}
           >
-            પરિવાર 
+            પરિવાર
           </Link>
-
-       
 
           <Link
             href="/gallery"
             className="text-lg font-medium py-2 px-4 rounded-lg hover:bg-[#ffffff] hover:text-primary transition-shadow hover:shadow-[0px_10px_10px_0px_#144F0F26,0px_4px_4px_0px_#00000040]"
             onClick={() => setMobileMenuOpen(false)}
           >
-            ગેલેરી 
+            ગેલેરી
           </Link>
-
-        
         </div>
       )}
     </header>
